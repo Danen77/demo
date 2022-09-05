48455130866e59856af35457b6c639451be8d4d9
Nenad Gacevic <nenad.gacevic@comdata.rs>
2022-09-05 03:53:29 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React, { useState, createContext } from 'react';
 import './App.css';
 import {
-  BrowserRouter,
-  Routes,
-  Route,
   Link,
   Outlet
 } from "react-router-dom";
-import FavoriteColor from './useStateExample/FavouriteColor';
-import Counter from './useStateExample/Counter'
-import Timer from './useEffectExample/Timer'
-import FetchData from './useEffectExample/DataFetchExample';
-import User from './useContextExample/User';
-import Stopwatch from './useReducerExample/Stopwatch';
-import CounterR from './useReducerExample/CounterR';
-import UseStateExample from './useStateExample/UseStateExample';
-import UseReducerExample from './useReducerExample/UseReducerExample';
-import UseEffectExample from './useEffectExample/UseEffectExample';
-// import User from './useContextExample/User';
 export const UserContext = createContext();
 
 const App = () => {
@@ -35,7 +21,7 @@ const App = () => {
           >
             <Link to="useState">useState Example</Link> |{" "}
             <Link to="useEffect">useEffect Example</Link> |{" "}
-            <Link to="/useContext">useContext Example</Link> |{" "}
+            <Link to="useContext">useContext Example</Link> |{" "}
             <Link to="useReducer">useReducer Example</Link> |{" "}
           </nav>
           <Outlet />
